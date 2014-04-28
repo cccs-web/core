1eb335543d35334586be6757a7e14b8cfa539e76
pwhipp <paul.whipp@gmail.com>
2014-04-29 09:35:53 +1000

    Set to Chicago timezone

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ ALLOWED_HOSTS = ['.crossculturalconsult.com']
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = 'America/Chicago'
 
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = True
