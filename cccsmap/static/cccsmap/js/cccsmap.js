4da8fb491068e3eba4d15b14e9bcba932e53c96d
pwhipp <paul.whipp@gmail.com>
2014-05-06 17:30:59 +1000

    Enhanced /maps/test/ to pick up villages points from the url

--- cccsmap/static/cccsmap/js/cccsmap.js
+++ cccsmap/static/cccsmap/js/cccsmap.js
@@ -1,8 +1,8 @@
 // POC Lots of work needed here - drawn from open layers geojson example
 
-var lon = 5;
-var lat = 40;
-var zoom = 5;
+var lon = 131.114123;
+var lat = -7.628902;
+var zoom = 8;
 var map, layer;
 
 $(document).ready(function(){
@@ -12,40 +12,17 @@ $(document).ready(function(){
         {layers: 'basic'} );
     map.addLayer(layer);
     map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
-    var featurecollection = {
-        "type": "FeatureCollection",
-        "features": [
-            {"geometry": {
-                "type": "GeometryCollection",
-                "geometries": [
-                    {
-                        "type": "LineString",
-                        "coordinates":
-                            [[11.0878902207, 45.1602390564],
-                                [15.01953125, 48.1298828125]]
-                    },
-                    {
-                        "type": "Polygon",
-                        "coordinates":
-                            [[[11.0878902207, 45.1602390564],
-                                [14.931640625, 40.9228515625],
-                                [0.8251953125, 41.0986328125],
-                                [7.63671875, 48.96484375],
-                                [11.0878902207, 45.1602390564]]]
-                    },
-                    {
-                        "type":"Point",
-                        "coordinates":[15.87646484375, 44.1748046875]
-                    }
-                ]
-            },
-                "type": "Feature",
-                "properties": {}}
-        ]
-    };
-    var geojson_format = new OpenLayers.Format.GeoJSON();
+
     var vector_layer = new OpenLayers.Layer.Vector();
     map.addLayer(vector_layer);
-    vector_layer.addFeatures(geojson_format.read(featurecollection));
 
-})
+    OpenLayers.Request.GET({
+        url: "/maps/villages/",
+        headers: {'Accept': 'application/json'},
+        success: function(req){
+            var g = new OpenLayers.Format.GeoJSON();
+            var feature_collection = g.read(req.responseText);
+            vector_layer.destroyFeatures();
+            vector_layer.addFeatures(feature_collection);
+        }
+    })})
