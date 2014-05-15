b6c31bf77ff978cd0141a96d999bfe64a892b8f0
pwhipp <paul.whipp@gmail.com>
2014-05-15 18:22:45 +1000

    sorted categorization lists and added sectors and countries

--- cvs/urls.py
+++ cvs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = patterns(
     url(r'^project/bycccstheme/$', views.ProjectCCCSThemeListView.as_view(), name='project-list-cccs-theme'),
     url(r'^project/bycccssector/$', views.ProjectCCCSSectorListView.as_view(), name='project-list-cccs-sector'),
     url(r'^project/byifctheme/$', views.ProjectIFCThemeListView.as_view(), name='project-list-ifc-theme'),
+    url(r'^project/bycountry/$', views.ProjectCountryListView.as_view(), name='project-list-country'),
+    url(r'^project/byifcsector/$', views.ProjectIFCSectorListView.as_view(), name='project-list-ifc-sector'),
     url(r'^project/(?P<slug>[-_\w]+)/$', views.ProjectDetailView.as_view(), name='project-detail'))
--- cvs/views.py
+++ cvs/views.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.core import serializers
@@ -19,12 +21,13 @@ class ProjectCCCSThemeListView(ListView):
     categorization_fieldname = 'cccs_subthemes'
     categorization_parent_fieldname = 'theme'
     categorization_label = 'CCCS Theme'
+    template_name = "cvs/project_list2.html"
 
     def get_context_data(self, **kwargs):
         context = super(ProjectCCCSThemeListView, self).get_context_data(**kwargs)
         context['categorization_name'] = self.categorization_label
         context['use_right_col'] = "No"  # a bit hacky but it will do for now
-        context['categorization'] = categorize_projects(context['object_list'],
+        context['categorization'] = categorize_projects2(context['object_list'],
                                                         self.categorization_fieldname,
                                                         self.categorization_parent_fieldname)
         return context
@@ -41,9 +44,46 @@ class ProjectCCCSSectorListView(ProjectCCCSThemeListView):
     categorization_label = 'CCCS Sector'
 
 
-def categorize_projects(projects, categorization_fieldname, categorization_parent_fieldname):
+class ProjectCountryListView(ListView):
+    model = cm.Project
+    categorization_fieldname = 'countries'
+    categorization_label = 'Country'
+    template_name = "cvs/project_list1.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectCountryListView, self).get_context_data(**kwargs)
+        context['categorization_name'] = self.categorization_label
+        context['use_right_col'] = "No"  # a bit hacky but it will do for now
+        context['categorization'] = categorize_projects(context['object_list'],
+                                                        self.categorization_fieldname)
+        return context
+
+
+class ProjectIFCSectorListView(ProjectCountryListView):
+    categorization_fieldname = 'ifc_sectors'
+    categorization_label = 'IFC Sector'
+
+
+def categorize_projects(projects, categorization_fieldname):
     """
-    Organise the projects so that they are nested in the ifc theme labels
+    Organise the projects using a single categorization layer
+    """
+    categorization = dict()
+    for project in projects:
+        categories = getattr(project, categorization_fieldname).all()
+        for category in categories:
+            category_name = category.name
+            if category_name not in categorization:
+                categorization[category_name] = dict(projects=list(), count=0)
+            categorization[category_name]['count'] += 1
+            categorization[category_name]['id'] = category.id
+            categorization[category_name]['projects'].append(project)
+    return OrderedDict(((k, categorization[k]) for k in sorted(categorization.keys())))
+
+
+def categorize_projects2(projects, categorization_fieldname, categorization_parent_fieldname):
+    """
+    Organise the projects so that they are nested in the sub categorizations
     """
     categorization = dict()
     for project in projects:
@@ -65,6 +105,8 @@ def categorize_projects(projects, categorization_fieldname, categorization_paren
             categorization[super_name]['subs'][sub_name]['count'] += 1
             categorization[super_name]['subs'][sub_name]['id'] = sub.id
 
-    return categorization
+    for info in categorization.values():
+        info['subs'] = OrderedDict(((k, info['subs'][k]) for k in sorted(info['subs'].keys())))
+    return OrderedDict(((k, categorization[k]) for k in sorted(categorization.keys())))
 
 
