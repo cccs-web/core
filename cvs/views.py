68fc343d4693409b179dd69d5fd7bf5abab67081
pwhipp <paul.whipp@gmail.com>
2014-05-22 12:36:59 +1000

    Set visibility of cv edit button so it only shows for staff or the correct user

--- cvs/views.py
+++ cvs/views.py
@@ -17,17 +17,27 @@ class CVListView(ListView):
         return context
 
 
-class CVDetailView(CCCSDetailView):
+class CVDetailMixin(object):
     model = cm.CV
 
+    def can_update(self, request):
+        return (request.user == self.get_object().user) or request.user.is_staff
 
-class CVUpdateView(UpdateView):
-    model = cm.CV
+
+class CVDetailView(CVDetailMixin, CCCSDetailView):
+
+    def get_context_data(self, **kwargs):
+        context = super(CVDetailView, self).get_context_data(**kwargs)
+        context['can_update'] = self.can_update(self.request)
+        return context
+
+
+class CVUpdateView(CVDetailMixin, UpdateView):
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
             return redirect('/accounts/login?next={0}'.format(request.path))
-        elif (request.user == self.get_object().user) or request.user.is_staff:
+        elif self.can_update(request):
             return super(CVUpdateView, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
