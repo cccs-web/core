a20a280400791a540c1bed20161f26f4a7368e97
pwhipp <paul.whipp@gmail.com>
2014-05-23 09:57:03 +1000

    Added front editing for adding, deleting and modifying cvprojects.

--- cvs/views.py
+++ cvs/views.py
@@ -2,6 +2,8 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect
+from django.forms.models import inlineformset_factory
+from django.http.response import HttpResponseRedirect
 
 from projects.views import CCCSDetailView
 
@@ -29,6 +31,9 @@ class CVDetailView(CVDetailMixin, CCCSDetailView):
         return context
 
 
+CVProjectFormSet = inlineformset_factory(cm.CV, cm.CVProject)
+
+
 class CVUpdateView(CVDetailMixin, UpdateView):
 
     def dispatch(self, request, *args, **kwargs):
@@ -38,6 +43,36 @@ class CVUpdateView(CVDetailMixin, UpdateView):
             return super(CVUpdateView, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def get_context_data(self, **kwargs):
+        context = super(CVUpdateView, self).get_context_data(**kwargs)
+        context['use_right_col'] = "No"
+        context['cvproject_formset'] = self.get_cvproject_formset()
+        return context
+
+    def get_cvproject_formset(self):
+        cvproject_formset_kwargs = {
+            'instance': self.object,
+            'queryset': cm.CVProject.objects.filter(cv=self.object)}
+        if self.request.method == 'POST':
+            cvproject_formset = CVProjectFormSet(self.request.POST, **cvproject_formset_kwargs)
+        else:
+            cvproject_formset = CVProjectFormSet(**cvproject_formset_kwargs)
+        return cvproject_formset
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        cvproject_formset = context['cvproject_formset']
+        if cvproject_formset.is_valid():
+            form.save()
+            cvproject_formset.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return super(CVUpdateView, self).form_valid(form)
+
+
+
+
 
 
 
