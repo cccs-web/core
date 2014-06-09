acd42d2fc51aca0906d3ffa1eab0b30d895ece8e
pwhipp <paul.whipp@gmail.com>
2014-06-09 14:44:27 +1000

    fixed dev.py

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,1 +1,15 @@
-/home/paul/Dropbox/backup/cccs/local_settings.py
+from .base import *
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "cccs",
+        "USER": "cccs",
+        "PASSWORD": DBPASSWORD,
+        "HOST": "",
+        "PORT": ""}}
+
+VIRTUALENV = 'cccs'
