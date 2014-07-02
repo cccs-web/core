4d9206ff6d8b54e2a289b0cf73c072aa15b32156
pwhipp <paul.whipp@gmail.com>
2014-07-02 15:27:25 +1000

    working with cccs_web fork of https://github.com/Kniyl/mezzanine

---
+++ core/settings/dev_multilingual.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "cccs_multilingual",
+        "USER": "cccs",
+        "PASSWORD": DBPASSWORD,
+        "HOST": "",
+        "PORT": ""}}
+
+VIRTUALENV = 'cccs'
