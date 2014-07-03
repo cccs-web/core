731674fa22d02f3fabb71f6da51302eb7ea1a509
pwhipp <paul.whipp@gmail.com>
2014-07-03 15:25:55 +1000

    Added model translation settings and initial translation model for projects (translations are in place for mezzanine but special db sync will be needed to add the necessary fields).

--- core/settings/dev_multilingual.py
+++ core/settings/dev_multilingual.py
@@ -13,3 +13,23 @@ DATABASES = {
         "PORT": ""}}
 
 VIRTUALENV = 'cccs'
+
+USE_MODELTRANSLATION = True
+
+USE_I18N = True
+
+from django.utils.translation import ugettext_lazy as _
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('de', _('German')),
+    ('es', _('Spanish'),
+    ('fr', _('French'))),
+    ('id', _('Indonesian')))
+
+# Keep slugs etc. in English when generated automatically
+MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'en'
+
+# Using a single translation.py in core for everything for now (rather than stick these in each app)
+MODELTRANSLATION_TRANSLATION_FILES = (
+    'core.translation',)
