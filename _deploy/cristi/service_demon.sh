766042bba74bab560bdb5479315bd7a51a003b08
pwhipp <paul.whipp@gmail.com>
2014-07-09 11:56:32 +1000

    Added cristy deployment configuration files

---
+++ _deploy/cristi/service_demon.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run the gunicorn service
+
+# Make sure we're in the right virtual env and location
+source /home/cristi/.virtualenvs/cccs/bin/activate
+source /home/cristi/.virtualenvs/cccs/bin/postactivate
+
+cd /home/cristi/production
+
+exec gunicorn -c /home/cristi/cccs_web/_deploy/gunicorn.conf.py core.wsgi:application
