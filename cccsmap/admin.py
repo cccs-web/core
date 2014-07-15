55aa6d53e4b0dfeb60bfe37980f4dd90cb497e87
pwhipp <paul.whipp@gmail.com>
2014-07-15 17:48:11 +1000

    Added filtering and sorting of test village data for #71

--- cccsmap/admin.py
+++ cccsmap/admin.py
@@ -5,5 +5,6 @@ from models import Village
 
 class VillageAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'population', 'provinsi', 'kecamatan', 'desa', 'quartile')
+    list_filter = ('provinsi', 'kecamatan', 'desa')
 
 admin.site.register(Village, VillageAdmin)
