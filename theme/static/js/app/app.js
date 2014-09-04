4c68354bf0cdd4b6765791f1b338fa9e4e1dca3b
Cristian Maghiar <cristi@avangard-media.ro>
2014-09-04 20:43:37 +0300

    slides tweaks

--- theme/static/js/app/app.js
+++ theme/static/js/app/app.js
@@ -2,7 +2,7 @@ require([
     'jquery'
     , 'jqueryui'
     , 'browser_check'
-    , 'dom_shim'
+    // , 'dom_shim'
     , 'fastclick'
     , 'foundation.abide'
     , 'foundation.accordion'
@@ -83,15 +83,15 @@ require([
             },
 
             orbit: {
-                animation: 'slide', // Sets the type of animation used for transitioning between slides, can also be 'fade'
+                animation: 'fade', // Sets the type of animation used for transitioning between slides, can also be 'fade'
                 timer_speed: 10000, // Sets the amount of time in milliseconds before transitioning a slide
                 pause_on_hover: true, // Pauses on the current slide while hovering
-                resume_on_mouseout: false, // If pause on hover is set to true, this setting resumes playback after mousing out of slide
+                resume_on_mouseout: true, // If pause on hover is set to true, this setting resumes playback after mousing out of slide
                 next_on_click: true, // Advance to next slide on click
                 animation_speed: 500, // Sets the amount of time in milliseconds the transition between slides will last
                 stack_on_small: false,
                 navigation_arrows: true,
-                slide_number: true,
+                slide_number: false,
                 slide_number_text: 'of',
                 container_class: 'orbit-container',
                 stack_on_small_class: 'orbit-stack-on-small',
