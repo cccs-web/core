70612f7218afcd36d4cf67bb252cd5c853544b7b
pwhipp <paul.whipp@gmail.com>
2014-10-01 18:21:25 +1000

    Tidied up presentation and added basic saving (#75)

--- documents/urls.py
+++ documents/urls.py
@@ -7,4 +7,5 @@ import documents.views as views
 urlpatterns = patterns(
     '',
     url(r'^list/$', views.DocumentListView.as_view(), name='document-list'),
-    url(r'^detail/(?P<slug>[-_\w]+)/$', views.DocumentDetailView.as_view(), name='document-detail'))
+    url(r'^detail/(?P<slug>[-_\w]+)/$', views.DocumentDetailView.as_view(), name='document-detail'),
+    url(r'^download/(?P<slug>[-_\w]+)/$', views.download, name='document-download'))
--- documents/views.py
+++ documents/views.py
@@ -1,8 +1,9 @@
-from django.http.response import Http404
+import os
+
+from django.http.response import Http404, HttpResponse
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-
-from mezzanine.utils.views import paginate
+from django.shortcuts import get_object_or_404
 
 import documents.models as dm
 
@@ -18,7 +19,6 @@ class DocumentListView(ListView):
         return queryset
 
 
-
 class DocumentDetailView(DetailView):
     model = dm.Document
 
@@ -27,3 +27,20 @@ class DocumentDetailView(DetailView):
         if not self.request.user.is_staff and obj.status != dm.CONTENT_STATUS_PUBLISHED:
             raise Http404
         return obj
+
+
+def download(request, slug):
+    """
+    Download the file
+    :param request:
+    :param slug:
+    :return: response
+    """
+
+    document = get_object_or_404(dm.Document, slug=slug)
+    filename = os.path.basename(document.source_file.name)
+
+    response = HttpResponse(document.source_file, content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
+
+    return response
