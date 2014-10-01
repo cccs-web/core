84c6d95da35c9d578423aede90149942d1c9d3f2
pwhipp <paul.whipp@gmail.com>
2014-10-01 17:34:05 +1000

    Added document list pagination (#75)

--- documents/views.py
+++ documents/views.py
@@ -2,17 +2,21 @@ from django.http.response import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from mezzanine.utils.views import paginate
+
 import documents.models as dm
 
 
 class DocumentListView(ListView):
     model = dm.Document
+    paginate_by = 25
 
     def get_queryset(self):
         queryset = super(DocumentListView, self).get_queryset()
         if not self.request.user.is_staff:
             queryset = queryset.filter(status=dm.CONTENT_STATUS_PUBLISHED)
         return queryset
+
 
 
 class DocumentDetailView(DetailView):
