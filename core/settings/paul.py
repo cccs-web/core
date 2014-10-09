a23e448a5682297405e7266fb7aeb979c987b9a1
pwhipp <paul.whipp@gmail.com>
2014-10-09 17:54:21 +1000

    Reroute paul.py settings to cccs-docs for further testing.

--- core/settings/paul.py
+++ core/settings/paul.py
@@ -3,4 +3,5 @@ from .dev import *
 SITE_TITLE = 'Local Cross-Cultural Consulting'
 SITE_TAGLINE = None
 
-AWS_STORAGE_BUCKET_NAME = "pwc-cccs-dev"
+# AWS_STORAGE_BUCKET_NAME = "pwc-cccs-dev"
+AWS_STORAGE_BUCKET_NAME = "cccs-docs"
