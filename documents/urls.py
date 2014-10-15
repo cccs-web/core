e7dc025e10ff1f8fd759d8f307b0967e57938f9e
pwhipp <paul.whipp@gmail.com>
2014-10-15 17:03:38 +1000

    Created test for bibtex using Angular framework (#181)

--- documents/urls.py
+++ documents/urls.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 import documents.views as views
 
@@ -10,4 +11,5 @@ urlpatterns = patterns(
     url(r'^category/$', views.RootCategoriesView.as_view(), name='document-category-root'),
     url(r'^category/(?P<category_slugs>[\w/-]+)/$', views.CategoryView.as_view(), name='document-category'),
     url(r'^detail/(?P<slug>[\w_-]+)/$', views.DocumentDetailView.as_view(), name='document-detail'),
+    url(r'^bibtex/$$', TemplateView.as_view(template_name='documents/document_bibtex.html'), name='document-bibtex'),
     url(r'^download/(?P<slug>[\w_-]+)/$', views.download, name='document-download'))
