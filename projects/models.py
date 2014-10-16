db209e6f9a6d03a7f625e5df372f1201eefac12e
pwhipp <paul.whipp@gmail.com>
2014-10-16 10:49:34 +1000

    Added SubProject model (#120)

--- projects/models.py
+++ projects/models.py
@@ -209,3 +209,11 @@ class Project(RichText, Displayable):
 
 # Override inherited verbose names
 Project._meta.get_field('short_url').verbose_name = 'Short URL'
+
+
+class SubProject(models.Model):
+    project = models.ForeignKey(Project)
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        unique_together = ('project', 'name')
