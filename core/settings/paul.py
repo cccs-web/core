9fca348e02772ede9bf86cce9580afae6841b64c
pwhipp <paul.whipp@gmail.com>
2014-10-26 10:09:17 +1000

    Ease S3 switching for testing (#187)

--- core/settings/paul.py
+++ core/settings/paul.py
@@ -3,5 +3,8 @@ from .dev import *
 SITE_TITLE = 'Local Cross-Cultural Consulting'
 SITE_TAGLINE = None
 
-# AWS_STORAGE_BUCKET_NAME = "pwc-cccs-dev"
-AWS_STORAGE_BUCKET_NAME = "cccs-docs"
+# Overwrite AWS settings depending upon what I'm up to
+# AWS_OVERRIDES is set in my secrets.py. It overrides
+# AWS_STORAGE_BUCKET_NAME, AWS_ACCESS_KEY_ID, and AWS_SECRET_ACCESS_KEY
+# Can't do this in secrets.py because other settings set the values.
+globals().update(AWS_OVERRIDES)
