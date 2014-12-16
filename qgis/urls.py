1fb799fce7153dec7c411e2f2169b05f6b1df265
pwhipp <paul.whipp@gmail.com>
2014-12-16 16:58:21 +1000

    Updated qgis presentation to use multiple project servers

--- qgis/urls.py
+++ qgis/urls.py
@@ -7,4 +7,4 @@ import qgis.views as qv
 urlpatterns = patterns(
     '',
     url(r'^$', qv.HomeView.as_view(), name='qgis-home'),
-    url(r'^project/(?P<file_name>[\.-_\w]+)$', qv.QGISProjectView.as_view(), name='qgis-project'))
+    url(r'^project/(?P<project_name>[\.-_\w]+)$', qv.QGISProjectView.as_view(), name='qgis-project'))
